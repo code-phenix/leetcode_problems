99d293f930a31a0e6e32e8bbbcb4048a71263d10
codepheonix <55018311+code-phenix@users.noreply.github.com>
2022-01-04 19:44:22 +0530

    Create Unique Morse Code Words.java

---
+++ Unique Morse Code Words.java
@@ -0,0 +1,54 @@
+// International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+// 'a' maps to ".-",
+// 'b' maps to "-...",
+// 'c' maps to "-.-.", and so on.
+// For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+// [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+// Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+// For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+// Return the number of different transformations among all words we have.
+
+ 
+
+// Example 1:
+
+// Input: words = ["gin","zen","gig","msg"]
+// Output: 2
+// Explanation: The transformation of each word is:
+// "gin" -> "--...-."
+// "zen" -> "--...-."
+// "gig" -> "--...--."
+// "msg" -> "--...--."
+// There are 2 different transformations: "--...-." and "--...--.".
+// Example 2:
+
+// Input: words = ["a"]
+// Output: 1
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 12
+// words[i] consists of lowercase English letters.
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String arr[]=new String[]{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Set<String> hs=new HashSet();
+        for(String iterI:words){
+            StringBuilder temp=new StringBuilder();
+            for(int iterJ=0;iterJ<iterI.length();iterJ++){
+                temp.append(arr[iterI.charAt(iterJ)-'a']);
+            }
+            hs.add(temp.toString());
+        }
+        return hs.size();
+    }
+}
+
+//Runtime: 1 ms, faster than 100.00% of Java online submissions for Unique Morse Code Words.
+//Memory Usage: 36.9 MB, less than 81.06% of Java online submissions for Unique Morse Code Words.
