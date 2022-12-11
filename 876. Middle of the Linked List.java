30ad074b1c0ea43b81bf37b2c40b3a6b885b0439
codepheonix <55018311+code-phenix@users.noreply.github.com>
2022-12-11 20:49:15 +0530

    Create 876. Middle of the Linked List.java

---
+++ 876. Middle of the Linked List.java
@@ -0,0 +1,61 @@
+// Given the head of a singly linked list, return the middle node of the linked list.
+
+// If there are two middle nodes, return the second middle node.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+// Example 2:
+
+
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 100].
+// 1 <= Node.val <= 100
+// Accepted
+// 1.1M
+// Submissions
+// 1.5M
+// Acceptance Rate
+// 74.6%
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slowPointer=head;
+        ListNode fastPointer=head;
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next.next;
+        }
+        return slowPointer;
+    }
+}
+
+// Runtime
+// 0 ms
+// Beats
+// 100%
+// Memory
+// 41.5 MB
+// Beats
+// 58.1%
